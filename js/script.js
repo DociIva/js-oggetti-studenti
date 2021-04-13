f3378c3b74e3c49f5fc142b0b5f06c018351328f
DociIva <doci.iva@hotmail.com>
2021-04-13 15:49:12 +0200

    primo p. secondo p.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-console.log('Ciao mondo');
+//console.log('Ciao mondo');
 /**
- * Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
- *  Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
- Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
- Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. 
- Una volta creato il nuovo oggetto inserirlo nell'array creato al punto 2 e mostrare alla fine il contenuto dell'array.
+ * Creare un oggetto che descriva uno studente, con le seguenti proprietà:
+ *  nome, cognome e età.
+ *  Stampare a schermo attraverso un ciclo for-in tutte 
+ * le proprietà dell'oggetto.
+ Creare un array di oggetti di studenti. Ciclare su tutti gli studenti
+  e stampare per ognuno di essi, nome e cognome.
+ Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere
+  un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. 
+ Una volta creato il nuovo oggetto inserirlo nell'array creato al punto
+  2 e mostrare alla fine il contenuto dell'array.
  * 
  */
+// 1. creiamo un oggeto che desciva uno studente, con q.p. nome,cognome,età
+var studente = {
+    'nome'  : 'Iva',
+    'cognome' : 'Doci',
+    'eta' : '25',
+};
+//console.log(studente.nome);
+
+// 2. stampa tutti gli elementi con ciclo for-in
+for(var key in studente) {
+    console.log(studente[key]);
+}
